ed279717eacf8e8a35d7477e5e9a2cccce4234f0
刘俊 <liujun@meizu.com>
2016-04-15 11:17:04 +0800

    add buildConfigField example, and task description

--- BasicProject/app/src/main/java/com/meizu/sample/basicproject/MainActivity.java
+++ BasicProject/app/src/main/java/com/meizu/sample/basicproject/MainActivity.java
@@ -9,6 +9,8 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.io.File;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -17,7 +19,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
